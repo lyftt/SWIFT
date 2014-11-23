37e6c467d2de4d65bdbf5f24985c0581dd0c5145
Xingtong Zhou <xingtong@umich.edu>
2014-11-23 18:07:41 -0500

    clone every inst, hasn't resolve dependency

--- lib/swift/swift.cpp
+++ lib/swift/swift.cpp
@@ -17,8 +17,16 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Analysis/ProfileInfo.h"
 #include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/IntrinsicInst.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Metadata.h"
 
 /* Header file global to this project */
+#include <map>
 
 using namespace llvm;
 
@@ -35,15 +43,40 @@ namespace {
 			// Destructor, TBD
 			
 		}
+		
+		std::map<Value *, Value *> shadowMap;
 
 		virtual bool runOnFunction(Function &F) {
 	
-			for (Function::iterator b = F.begin(); b != F.end(); b++) {
-				// iterate through each BasicBlock b
+			for (Function::iterator BB = F.begin(), BE = F.end(); BB != BE; BB++) {
+				// iterate through each BasicBlock BB
 				
-				for (BasicBlock::iterator i = b->begin(); i != b->end(); i++) {
+				for (BasicBlock::iterator II = BB->begin(), IE = BB->end(); II != IE; ) {
 					// iterate through each Instruction i
+					Instruction &I = *II++;
 					
+					if (AllocaInst *AI = dyn_cast<AllocaInst>(&I)) {
+						//TODO alloca inst
+						errs() << "alloca inst: " << *AI << "\n";
+					
+					} else if (StoreInst *ST = dyn_cast<StoreInst>(&I)) {
+						//TODO store inst
+						errs() << "store inst: " << *ST << "\n";
+
+					} else if (BranchInst *BR = dyn_cast<BranchInst>(&I)) {
+						//TODO branch inst
+						errs() << "branch inst: " << *BR << "\n";
+						
+					} else if (TerminatorInst *TI = dyn_cast<TerminatorInst>(&I)) {
+						//TODO terminator inst
+						errs() << "terminator: " << *TI << "\n";
+						
+					} else {
+						// regular inst
+						errs() << "regular inst: " << I << "\n";
+						Instruction *cloned = I.clone();
+						cloned->insertAfter(&I);
+					}
 				}
 			}
 			
