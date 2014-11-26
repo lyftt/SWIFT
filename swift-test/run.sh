6fa2bb887861075f19f1c1d751ba3315cdb66afa
Xingtong Zhou <xingtong@umich.edu>
2014-11-25 20:51:02 -0500

    modify run.sh to add execution time and static size

--- swift-test/run.sh
+++ swift-test/run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 fname=test
-proj_dir=~/swift
+proj_dir=~/SWIFT
 
 rm *.bc *.s *.dot *.exe
 
 clang -emit-llvm -o $fname.bc -c $fname.c || { echo "Failed to emit llvm bc"; exit 1; }
+size1=$(stat -c %s $fname.bc)
+llc $fname.bc -o $fname.s
+g++ -o $fname.exe $fname.s
+t1=$(date +%s%N)
+./$fname.exe
+t2=$(date +%s%N)
 
 opt -gvn < $fname.bc > $fname.opt.bc || { echo "Failed to opt-load"; exit 1; }
 
@@ -12,14 +18,21 @@ opt -dot-cfg $fname.opt.bc
 mv cfg.main.dot non-swift-cfg.dot
 dot -Tpdf non-swift-cfg.dot -o $fname-non-swift-cfg.pdf
 
-opt -load $proj_dir/Release+Asserts/lib/swift.so -swift < $fname.opt.bc > $fname.swift.bc || { echo "Failed to opt-load"; exit 1; }
+opt -load $proj_dir/Debug+Asserts/lib/swift.so -swift < $fname.opt.bc > $fname.swift.bc || { echo "Failed to opt-load"; exit 1; }
+size2=$(stat -c %s $fname.swift.bc)
 
 llc $fname.swift.bc -o $fname.swift.s
 g++ -o $fname.swift.exe $fname.swift.s
-./$fname.swift.exe
+t3=$(date +%s%N)
+ret=$(./$fname.swift.exe)
+t4=$(date +%s%N)
 
 opt -dot-cfg $fname.swift.bc
 mv cfg.main.dot swift-cfg.dot
 dot -Tpdf swift-cfg.dot -o $fname-swift-cfg.pdf
 
 rm *.bc *.s *.dot *.exe
+
+echo "scale=3;"$(($t4-$t3))/$(($t2-$t1)) | bc
+echo "scale=3;"$size2/$size1 | bc
+echo $ret
